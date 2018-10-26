39d735cc5ec545d743fed439efe440e7211fbdb8
wangruizzz <15735655464@163.com>
2018-10-26 14:36:21 +0800

    444

--- meta.js
+++ meta.js
@@ -128,33 +128,11 @@ module.exports = {
         },
       ],
     },
-    e2e: {
-      when: 'isNotTest',
-      type: 'confirm',
-      message: 'Setup e2e tests with Nightwatch?',
-    },
     autoInstall: {
       when: 'isNotTest',
       type: 'list',
       message:
-        'Should we run `npm install` for you after the project has been created? (recommended)',
-      choices: [
-        {
-          name: 'Yes, use NPM',
-          value: 'npm',
-          short: 'npm',
-        },
-        {
-          name: 'Yes, use Yarn',
-          value: 'yarn',
-          short: 'yarn',
-        },
-        {
-          name: 'No, I will handle that myself',
-          value: false,
-          short: 'no',
-        },
-      ],
+        'Welcome to use vx cli'
     },
   },
   filters: {
